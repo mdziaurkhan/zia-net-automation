a205c8555f5c3bb4c08ffe3d6811be267385b030
mdziaurkhan <mdziaurkhan@gmail.com>
2020-10-15 23:38:14 -0500

    working version for ansible lab 8

--- ansible_zia/ansible-8_re-using_Ansible_artifacts/ansible-8_playbook.py
+++ ansible_zia/ansible-8_re-using_Ansible_artifacts/ansible-8_playbook.py
@@ -5,7 +5,12 @@
   - name: "importing project-2 playbook"
     import_playbook: /home/mdziaurkhan/zia-net-automation/ansible_zia/ansible-2_loopback_ospf/ansible-2_loopback-ospf-playbook.yaml
 
-  #- import_tasks: ansible-8_task.py
-    #hosts: routers
-    #vars:
-       #import_vars: /home/mdziaurkhan/zia-net-automation/ansible_zia/ansible-1_Connecting_devices/group_vars/routers
+  - hosts: routers
+    gather_facts: no
+
+    tasks:
+       - include_vars:
+            file: /home/mdziaurkhan/zia-net-automation/ansible_zia/ansible-1_Connecting_devices/group_vars/routers
+       - include_tasks: ansible-8_task.py
+         vars:
+            user_created_by_lookup: {'admin1':'admin123!', 'admin2':'admin123!' }
--- ansible_zia/ansible-8_re-using_Ansible_artifacts/ansible-8_task.py
+++ ansible_zia/ansible-8_re-using_Ansible_artifacts/ansible-8_task.py
@@ -1,8 +1,6 @@
-  vars:
-    user_created_by_lookup: {'admin1':'admin123!', 'admin2':'admin123!' }     # dictionary
-
   - name: "Configuring 2 users using with_<lookup> and dictionary"
     ios_config:
       lines:
         - "username {{item.key}} privilege 15 secret {{item.value}}"
-    with_dict: "{{ user_created_by_lookup }
+        #- "username test privilege 15 secret zia123"
+    with_dict: "{{ user_created_by_lookup }}"
